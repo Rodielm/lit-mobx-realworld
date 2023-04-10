ee5e1fc7cd1be0abba54efcf569412248d537827
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-04-10 23:34:02 +0200

    added vite config babel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
+  plugins: [
+    babel({
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: [
+          ['@babel/plugin-proposal-decorators', {
+            "legacy": true
+          }],
+          ["@babel/plugin-proposal-class-properties"]
+        ]
+      }
+    })
+  ],
   build: {
     lib: {
       entry: 'src/app.js',
