270b5155329fdc3c4130a3a070058a451db90330
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-04-10 23:43:26 +0200

    added base component

---
+++ src/components/base.js
@@ -0,0 +1,21 @@
+
+import { MobxReactionUpdate } from "@adobe/lit-mobx";
+import { LitElement, html } from "lit";
+import { classMap } from "lit-html/directives/class-map.js"
+import { styleMap } from "lit-html/directives/style-map.js"
+import { repeat } from "lit-html/directives/repeat.js"
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js"
+import { ifDefined } from "lit-html/directives/if-defined.js"
+import { withContext } from "wc-context";
+
+
+
+// This a base component to use with others components
+class Component extends withContext(MobxReactionUpdate(LitElement)) {
+    createRenderRoot() {
+        return this
+    }
+}
+
+
+export { Component, html, styleMap, classMap, repeat, unsafeHTML, ifDefined }
