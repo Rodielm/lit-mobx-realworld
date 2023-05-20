e870c26d08f092eb95c4e8b55adcd3c3110eb973
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-05-20 16:05:30 +0200

    add home-page

---
+++ src/pages/Home/home-main-view.js
@@ -0,0 +1,129 @@
+import ArticleList from '../../components/ArticleList'
+import { Component, html } from '../../components/base'
+import { withRouterLinks } from 'slick-router/middlewares/router-links'
+
+const YourFeedTab = ({ currentUser, tab }) => {
+  if (currentUser) {
+    return html`
+      <li class="nav-item">
+        <a
+          route="home"
+          query-tab="feed"
+          class=${`nav-link ${tab === 'feed' ? 'active' : ''}`}>
+          Your Feed
+        </a>
+      </li>
+    `
+  }
+  return null
+}
+
+const GlobalFeedTab = ({ tab }) => {
+  return html`
+    <li class="nav-item">
+      <a
+        route="home"
+        query-tab="all"
+        active-class=""
+        class=${`nav-link ${tab === 'all' ? 'active' : ''}`}>
+        Global Feed
+      </a>
+    </li>
+  `
+}
+
+const TagFilterTab = props => {
+  if (!props.tag) {
+    return null
+  }
+
+  return html`
+    <li class="nav-item">
+      <a route="home" query-tag=${props.tag} class="nav-link active">
+        <i class="ion-pound"></i>
+        ${props.tag}
+      </a>
+    </li>
+  `
+}
+
+@withRouterLinks
+class MainView extends Component {
+  static observedContexts = ['stores']
+
+  static properties = {
+    tab: { type: String },
+    tag: { type: String },
+  }
+
+  connectedCallback() {
+    super.connectedCallback()
+    this.stores.articlesStore.setPredicate(this.getPredicate())
+    this.stores.articlesStore.loadArticles()
+  }
+
+  shouldUpdate(changedProperties) {
+    if (changedProperties.has('tab') || changedProperties.has('tag')) {
+      this.stores.articlesStore.setPredicate(this.getPredicate())
+      this.stores.articlesStore.loadArticles()
+    }
+    return true
+  }
+
+  getTag() {
+    return this.tag || ''
+  }
+
+  getTab() {
+    return this.tab || 'all'
+  }
+
+  getPredicate() {
+    switch (this.getTab()) {
+      case 'feed':
+        return { myFeed: true }
+      case 'tag':
+        return { tag: this.tag }
+      default:
+        return {}
+    }
+  }
+
+  handleTabChange = tab => {
+    if (this.tab === tab) return
+    this.dispatchEvent(new CustomEvent('tab-changed', { detail: tab }))
+  }
+
+  handleSetPage = page => {
+    this.stores.articlesStore.setPage(page)
+    this.stores.articlesStore.loadArticles()
+  }
+
+  render() {
+    const { currentUser } = this.stores.userStore
+    const { articles, isLoading, page, totalPagesCount } =
+      this.stores.articlesStore
+
+    const tab = this.getTab()
+
+    return html`
+      <div>
+        <div class="feed-toggle">
+          <ul class="nav nav-pills outline-active" routerlinks>
+            ${YourFeedTab({ currentUser, tab })} ${GlobalFeedTab({ tab })}
+            ${TagFilterTab({ tag: this.tag })}
+          </ul>
+        </div>
+        ${ArticleList({
+          articles,
+          totalPagesCount,
+          loading: isLoading,
+          currentPage: page,
+          onSetPage: this.handleSetPage,
+        })}
+      </div>
+    `
+  }
+}
+
+customElements.define('home-main-view', MainView)
--- src/pages/Home/home-page.js
+++ src/pages/Home/home-page.js
@@ -1,14 +1,49 @@
 import { Component, html } from '../../components/base'
 import { withRouterLinks } from 'slick-router/middlewares/router-links'
+import Banner from './Banner'
+import Tags from './Tags'
+import './home-main-view'
 
 @withRouterLinks
 class HomePage extends Component {
   static observedContexts = ['stores']
 
+  static properties = {
+    $router: {},
+  }
+
+  // Invoked when a component is added to the document's DOM.
+  connectedCallback() {
+    super.connectedCallback()
+    this.stores.commonStore.loadTags()
+  }
+
+  tabChanged(e) {
+    this.$router.transitionTo('home', {}, { tab: e.detail })
+  }
+
   render() {
+    const { tags, token, appName } = this.stores.commonStore
+    const query = this.$route ? this.$route.query : {}
     return html`
-      <div class="container">
-        <h2>Home Page</h2>
+      <div class="home-page">
+        ${Banner({ token: token, appName: appName })}
+        <div class="container page">
+          <div class="row">
+            <home-main-view
+              class="col-md-9"
+              .tag="${query.tag}"
+              .tab="${query.tab}"
+              @tab-changed=${this.tabChanged}
+            ></home-main-view>
+            <div class="col-md-3">
+              <div class="sidebar">
+                <p>Popular Tags</p>
+                ${Tags({ tags })}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     `
   }
