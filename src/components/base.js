ab40513d16f393207b402e587ffe1dcc5dfb0533
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-05-20 16:11:24 +0200

    reformat base

--- src/components/base.js
+++ src/components/base.js
@@ -1,19 +1,26 @@
+import { MobxReactionUpdate } from '@adobe/lit-mobx';
+import { LitElement, html, nothing } from 'lit';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { styleMap } from 'lit-html/directives/style-map.js';
+import { repeat } from 'lit-html/directives/repeat.js';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { withContext } from 'wc-context';
 
-import { MobxReactionUpdate } from "@adobe/lit-mobx";
-import { LitElement, html } from "lit";
-import { classMap } from "lit-html/directives/class-map.js"
-import { styleMap } from "lit-html/directives/style-map.js"
-import { repeat } from "lit-html/directives/repeat.js"
-import { unsafeHTML } from "lit-html/directives/unsafe-html.js"
-import { ifDefined } from "lit-html/directives/if-defined.js"
-import { withContext } from "wc-context";
-
-// This a base component withContext and MobxReactionUpdate  
+// This a base component withContext and MobxReactionUpdate
 class Component extends withContext(MobxReactionUpdate(LitElement)) {
-    createRenderRoot() {
-        return this
-    }
+  createRenderRoot() {
+    return this;
+  }
 }
 
-
-export { Component, html, styleMap, classMap, repeat, unsafeHTML, ifDefined }
+export {
+  Component,
+  html,
+  nothing,
+  styleMap,
+  classMap,
+  repeat,
+  unsafeHTML,
+  ifDefined,
+};
