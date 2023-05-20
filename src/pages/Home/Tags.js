179481474034d3d51557a3c6bd523b3bca3d44d3
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-05-20 15:42:59 +0200

    add Tags

---
+++ src/pages/Home/Tags.js
@@ -0,0 +1,27 @@
+import { html } from 'lit-html'
+import '../../components/loading-spinner.js'
+
+const Tags = (props) => {
+  const tags = props.tags
+  if (tags) {
+    return html`
+      <div class="tag-list" routerlinks>
+        ${tags.map((tag) => {
+          return html`
+            <a
+              route="home"
+              query-tab="tag"
+              query-tag=${tag}
+              class="tag-default tag-pill"
+              >${tag}</a
+            >
+          `
+        })}
+      </div>
+    `
+  } else {
+    return html` <loading-spinner></loading-spinner> `
+  }
+}
+
+export default Tags
