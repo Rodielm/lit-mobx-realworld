f134e7113ce91d731dd15af30154b7996eb6d304
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-07-12 19:59:16 +0200

    reformat router

--- src/router.js
+++ src/router.js
@@ -1,52 +1,51 @@
-import { observe } from "mobx"
+import { observe } from 'mobx';
 import { Router } from 'slick-router';
-import { wc } from "slick-router/middlewares/wc";
+import { wc } from 'slick-router/middlewares/wc';
 import {
-    routerLinks,
-    withRouterLinks
-} from 'slick-router/middlewares/router-links'
-
+  routerLinks,
+  withRouterLinks,
+} from 'slick-router/middlewares/router-links';
 
 export function createRouter(stores) {
-    const appLoaded = new Promise(resolve => {
-        const disposer = observe(
-            stores.commonStore,
-            "appLoaded",
-            ({ newValue }) => {
-                if (newValue) {
-                    resolve()
-                    disposer()
-                }
-            }
-        )
-    })
-
-    const routes = [
-        {
-            name: "app",
-            path: "/",
-            beforeEnter: () => appLoaded,
-            children: [
-                {
-                    name: 'home',
-                    component: 'home-page',
-                    path: ''
-                },
-                {
-                    name: 'login',
-                    component: 'login-page'
-                },
-                { name: 'register', component: 'register-page' },
-                { name: 'editor', component: 'editor-page', path: 'editor/:slug?' },
-                { name: 'article', component: 'article-page', path: 'article/:id' },
-            ]
+  const appLoaded = new Promise(resolve => {
+    const disposer = observe(
+      stores.commonStore,
+      'appLoaded',
+      ({ newValue }) => {
+        if (newValue) {
+          resolve();
+          disposer();
         }
-    ]
+      }
+    );
+  });
 
-    const router = new Router({ routes, outlet: 'outlet-elm', log: true });
-    router.use(wc)
-    router.use(routerLinks)
-    return router;
+  const routes = [
+    {
+      name: 'app',
+      path: '/',
+      beforeEnter: () => appLoaded,
+      children: [
+        {
+          name: 'home',
+          component: 'home-page',
+          path: '',
+        },
+        {
+          name: 'login',
+          component: 'login-page',
+        },
+        { name: 'register', component: 'register-page' },
+        { name: 'editor', component: 'editor-page', path: 'editor/:slug?' },
+        { name: 'article', component: 'article-page', path: 'article/:id' },
+      ],
+    },
+  ];
+
+  const router = new Router({ routes, outlet: 'outlet-elm', log: true });
+  router.use(wc);
+  router.use(routerLinks);
+  return router;
 }
 
-export { withRouterLinks }
+export { withRouterLinks };
